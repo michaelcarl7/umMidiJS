1190ff6598a95aa2b5d9dca280083b149e6ef45b
Mike Carl <mikecarl@Mikes-MacBook-Pro-2.local>
2016-01-13 13:17:17 -0700

    test commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name umMidiJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the umMidiJsApp
+ */
+angular.module('umMidiJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name umMidiJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the umMidiJsApp
+ */
+angular.module('umMidiJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
